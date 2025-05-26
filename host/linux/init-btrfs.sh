7b8db492624957d3863163f372c9d5152a0705e0
jarsXk <fedrrr@gmail.com>
2025-05-26 21:56:12 +0300

    fix qbt

---
+++ host/linux/init-btrfs.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# common
+btrfs subvolume create @var-log
+btrfs subvolume create @var-tmp
+
+# snapshots
+btrfs subvolume create @snapshots
+mkdir -p ./@rootfs/.snapshots
+
+#data
+mkdir -p ./@rootfs/srv/data
+mkdir -p ./@rootfs/srv/data/local-download
+mkdir -p ./@rootfs/srv/data/local-junk
+mkdir -p ./@rootfs/srv/data/local-share
+mkdir -p ./@rootfs/srv/data/local-books
+mkdir -p ./@rootfs/srv/data/local-music
+mkdir -p ./@rootfs/srv/data/local-software
+mkdir -p ./@rootfs/srv/data/local-video/public
+mkdir -p ./@rootfs/srv/data/local-video/family
+mkdir -p ./@rootfs/srv/data/local-documents
+mkdir -p ./@rootfs/srv/data/local-photo
+mkdir -p ./@rootfs/srv/data/local-saves
+mkdir -p ./@rootfs/srv/data/local-backup
+
+#docker
+mkdir -p ./@rootfs/var/snap/docker/common/var-lib-docker
+btrfs subvolume create @var-lib-docker
+mkdir -p ./@rootfs/srv/docker
+btrfs subvolume create @srv-docker
+mkdir -p ./@rootfs/var/snap/docker/common/var-lib-docker/volumes
+mkdir -p ./@srv-docker/volumes
+mkdir -p ./@rootfs/var/snap/docker/common/assets
+mkdir -p ./@srv-docker/assets
+mkdir -p ./@rootfs/var/snap/docker/common/data
+
+
