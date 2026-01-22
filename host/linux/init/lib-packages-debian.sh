d458dc39fbc4c4e30ed73b70092d075742470b85
jarsXk <fedrrr@gmail.com>
2026-01-22 10:51:21 +0300

    Update lib-packages-debian.sh

--- host/linux/init/lib-packages-debian.sh
+++ host/linux/init/lib-packages-debian.sh
@@ -11,6 +11,7 @@ run_command "$PACKAGEMANAGER update" "Error updating"
 run_command "$PACKAGEMANAGER -y full-upgrade" "Error updating"
 
 # Installing packages
+log_message "DEBUG" "DEBIAN_VERSION <$DEBIAN_VERSION>"
 PACKAGE_LIST="micro mc htop openssh-server openssh-client ca-certificates bash tzdata netcat-openbsd curl wget zstd unzip sudo util-linux figlet dnsutils imagemagick locales"
 if [ $DEBIAN_VERSION >= 13 ]; then
   PACKAGE_LIST="$PACKAGE_LIST fastfetch"
@@ -18,9 +19,11 @@ fi
 log_message INFO "Installing packages <$PACKAGE_LIST>"
 run_command "$PACKAGEMANAGER -y install $PACKAGE_LIST" "Error installing"
 if [ $DEBIAN_VERSION < 13 ] ; then
-  if [ $(dpkg --print-architecture) = amd64 ]; then
+  ARCHITECTURE=$(dpkg --print-architecture)
+  log_message "DEBUG" "ARCHITECTURE <$ARCHITECTURE>"
+  if [ $ARCHITECTURE = amd64 ]; then
     DPKG_NAME="fastfetch-linux-$(dpkg --print-architecture).deb"
-  elif [ $(dpkg --print-architecture) = arm64 ]; then
+  elif [ $ARCHITECTURE = arm64 ]; then
     DPKG_NAME="fastfetch-linux-$(uname -m).deb"
   fi
   log_message "DEBUG" "DPKG_NAME <$DPKG_NAME>"
