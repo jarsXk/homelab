7db3cd242757faaadf4dffa7574109a6294ebdee
jarsXk <fedrrr@gmail.com>
2026-01-22 09:21:54 +0300

    add test

---
+++ host/linux/init/lib-tst.sh
@@ -0,0 +1,3 @@
+
+echo "!!Inside"
+TSTMES="!!Ok"
---
+++ host/linux/init/tst.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+INIT_REPO=https://raw.githubusercontent.com/jarsXk/homelab/main/host/linux
+
+echo !!Start
+
+wget -O - ${INIT_REPO}/init/lib-tst.sh | cat /dev/stdin
+wget -O - ${INIT_REPO}/init/lib-tst.sh | . /dev/stdin
+
+echo !! $TSTMES
+echo !!End
