80925a211867eb0475b428fb6706d9a6456fe6ff
jarsXk <fedrrr@gmail.com>
2026-02-03 18:14:52 +0300

    upd init script

--- host/linux/init/init-debian.sh
+++ host/linux/init/init-debian.sh
@@ -18,6 +18,74 @@ log_message INFO "Initial setup for Debian Metal, VM & LXC"
 . <(wget -qO- ${INIT_REPO}/host/linux/init/lib-init-env-debian.sh)
 . <(wget -qO- ${INIT_REPO}/host/linux/init/lib-init-packages-debian.sh)
 . <(wget -qO- ${INIT_REPO}/host/linux/init/lib-init-ssh.sh)
+
+create_group() {
+# create_group name is_system gid 
+  if [ $(cat /etc/group | grep "$1" | wc -c) = 0 ]; then
+    if [ $2 = yes ]; then
+      SYSTEM_GROUP="--system"
+    fi
+    if [ $# -gt 2 ]; then
+      GROUP_GID="-g $3"
+    fi  
+    run_command "groupadd $GROUP_GID $SYSTEM_GROUP $1" "Error adding group $1"
+  else
+    if [ $# -gt 2 ]; then
+      run_command "groupmod -g $3 $1" "Error modifying group $1"
+    fi
+  fi
+  log_message INFO "Group <$1 ($(cat /etc/group | grep "$1"))>"
+}
+
+create_user() {
+# create_user login uid gid sudo shell create_home is_system extra_grops
+  COMMAND=""
+  
+  if [ $(cat /etc/passwd | grep "$1" | wc -c) = 0 ]; then
+    COMMAND="useradd --uid $2 --gid $3"
+    if [ $# -gt 7 ] || [ $4 = yes ]; then
+      if [ $# -gt 7 ] && [ $4 = no ]; then
+        COMMAND="$COMMAND -G $8"
+      elif [ $# -gt 7 ] && [ $4 = yes ]; then
+        COMMAND="$COMMAND -G $8,sudo"
+      elif [ $# -eq 7 ] && [ $4 = yes ]; then
+        COMMAND="$COMMAND -G sudo"
+      fi
+    fi
+    COMMAND="$COMMAND --shell $5"
+    if [ $6 = yes ]; then
+      COMMAND="$COMMAND --create-home"
+    else
+      COMMAND="$COMMAND --no-create-home"
+    fi
+    if [ $7 = yes ]; then
+      COMMAND="$COMMAND --system"
+    fi
+    COMMAND="$COMMAND $1"
+  
+    run_command "$COMMAND" "Error adding user $1"
+  else
+    COMMAND="usermod --uid $2 --gid $3"
+    if [ $# -gt 7 ] || [ $4 = yes ]; then
+      if [ $# -gt 7 ] && [ $4 = no ]; then
+        COMMAND="$COMMAND -a -G $8"
+      elif [ $# -gt 7 ] && [ $4 = yes ]; then
+        COMMAND="$COMMAND -a -G $8,sudo"
+      elif [ $# -eq 7 ] && [ $4 = yes ]; then
+        COMMAND="$COMMAND -a -G sudo"
+      fi
+    fi
+  
+    run_command "$COMMAND $1" "Error adding user $1"
+  fi
+
+  log_message READ "Set password for user <$1>"
+  run_command "passwd $1" "Error adding user $1"
+  log_message INFO "User <$1 ($(cat /etc/passwd | grep "$1"))>"
+}
+
+. <(wget -qO- ${INIT_REPO}/host/linux/init/lib-init-docker-debian.sh)
+
 . <(wget -qO- ${INIT_REPO}/host/linux/init/lib-init-raw-debian.sh)
 
 exit 0
---
+++ host/linux/init/lib-init-docker-debian.sh
@@ -0,0 +1,61 @@
+
+# Installing docker
+while [ "$DOCKER" = "" ]; do
+  log_message READ "Install Docker native (d), snapd (s), no install (n) [d/s/n/c]> " -n
+  read -r ANSWER_DOCKER
+  case "$ANSWER_DOCKER" in
+    [Dd]* )
+      DOCKER=native
+    ;;
+    [Ss]* )
+      DOCKER=snapd
+    ;;
+    [Nn]* )
+      DOCKER=no
+    ;;
+    [Cc]* )
+      log_message INFO "Canceled setup"
+      exit 1
+    ;;
+    * )
+      log_message INFO "Invalid input. Enter d, s, n, or c."
+    ;;
+  esac
+done
+log_message DEBUG "Selected install Docker <$DOCKER>"
+
+if [ $DOCKER != no ]; then
+  log_message INFO "Installing Docker"
+  create_group docker yes 199
+
+  if [ $DOCKER = native ]; then
+    DOCKER_CONFIG_PATH=/etc/docker
+
+    run_command "wget -O- https://get.docker.com | sh" "Error installing docker"   
+  elif [ $DOCKER = snapd ]; then
+    DOCKER_CONFIG_PATH=/var/snap/docker/current/config
+
+    PACKAGE_LIST="snapd"
+    log_message INFO "Installing additional packages <$PACKAGE_LIST>"
+    run_command "apt-get -y install $PACKAGE_LIST" "Error installing docker"
+    SNAP_LIST="snapd docker"
+    log_message INFO "Installing snaps <$SNAP_LIST>"
+    run_command "snap install $SNAP_LIST" "Error installing docker"
+  fi
+
+  run_command "mkdir -p ${DOCKER_CONFIG_PATH}" "Error installing docker"
+  if [ -f ${DOCKER_CONFIG_PATH}/daemon.json ]; then
+    run_command "mv ${DOCKER_CONFIG_PATH}/daemon.json ${DOCKER_CONFIG_PATH}/daemon.json.bak" "Error installing docker" 
+  fi
+  run_command "wget -O ${DOCKER_CONFIG_PATH}/daemon.json ${INIT_REPO}/host/linux/init/download/docker/daemon.json" "Error installing docker"
+
+  if [ $DOCKER = native ]; then
+    run_command "systemctl restart docker" "Error installing docker"
+    sleep 2s
+    run_command "docker version" "Error installing docker"
+  elif [ $DOCKER = snapd ]; then
+    run_command "snap restart docker" "Error installing docker"
+    sleep 2s
+    run_command "/snap/bin/docker version" "Error installing docker"
+  fi
+fi
