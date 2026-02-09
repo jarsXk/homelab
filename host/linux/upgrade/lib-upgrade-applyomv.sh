3132add1b069ac6418fe7500249512ec18cc20b4
jarsXk <fedrrr@gmail.com>
2026-02-09 10:13:52 +0300

    Update lib-upgrade-applyomv.sh

--- host/linux/upgrade/lib-upgrade-applyomv.sh
+++ host/linux/upgrade/lib-upgrade-applyomv.sh
@@ -3,7 +3,7 @@ if [ $(command -v omv-rpc | wc -c) -gt 0 ]; then
   if [ $(cat /var/lib/openmediavault/dirtymodules.json | wc -c) -gt 3 ]; then
     log_message INFO "Reloading OMV modules"
     sleep 60s
-    run_command "omv-rpc -u admin "config" "applyChanges" "{ \"modules\": $(cat /var/lib/openmediavault/dirtymodules.json),\"force\": true }"" "Error reloading OMV modules"
+    run_command "omv-rpc -u admin 'config' 'applyChanges' '{ \"modules\": $(cat /var/lib/openmediavault/dirtymodules.json),\"force\": true }'" "Error reloading OMV modules"
   else
     log_message INFO "No reloading OMV modules needed"
   fi
