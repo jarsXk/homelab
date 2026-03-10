c6454d61851c7114319639d0bab48a1c39b0e733
jarsXk <fedrrr@gmail.com>
2026-03-10 10:46:02 +0300

    Update init-debian.sh

--- host/linux/init/init-debian.sh
+++ host/linux/init/init-debian.sh
@@ -16,7 +16,7 @@ log_message INFO "Initial setup for Debian Metal, VM & LXC"
 . <(wget -qO- ${INIT_REPO}/host/linux/lib/lib-checkroot.sh)
 . <(wget -qO- ${INIT_REPO}/host/linux/init/common/lib-init-env.sh)
 . <(wget -qO- ${INIT_REPO}/host/linux/init/debian/lib-init-env-debian.sh)
-. <(wget -qO- ${INIT_REPO}/host/linux/init/debian/lib-init-packages-proxmox.sh)
+. <(wget -qO- ${INIT_REPO}/host/linux/init/debian/lib-init-packages-debian.sh)
 . <(wget -qO- ${INIT_REPO}/host/linux/init/common/lib-init-serverrole.sh)
 
 . <(wget -qO- ${INIT_REPO}/host/linux/init/debian/lib-init-groups-base-debian.sh)
