792371c30893db54f25eed8e6c3a359d4c580900
jarsXk <fedrrr@gmail.com>
2026-03-10 10:53:09 +0300

    Update lib-init-serverrole.sh

--- host/linux/init/common/lib-init-serverrole.sh
+++ host/linux/init/common/lib-init-serverrole.sh
@@ -21,7 +21,7 @@ while [ "$PHYSICAL" = "" ]; do
 done
 log_message DEBUG "Selected install physical server <$PHYSICAL>"
 
-if [ "$SERVER_ROLE" = "" ]; do
+if [ "$SERVER_ROLE" = "" ]; then
   # Reading location
   while [ "$SERVER_ROLE" = "" ]; do
     log_message READ "Setup NAS host [y/n/c]> " -n
