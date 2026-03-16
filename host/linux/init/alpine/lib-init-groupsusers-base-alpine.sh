eeed2c58e6740e1978f9af4bc19d49e7b1da565d
jarsXk <95273554+jarsXk@users.noreply.github.com>
2026-03-16 16:31:01 +0300

    alpine init users

---
+++ host/linux/init/alpine/lib-init-groupsusers-base-alpine.sh
@@ -0,0 +1,49 @@
+
+create_group() {
+# create_group name is_system gid 
+  if [ $(cat /etc/group | grep "$1" | wc -c) != 0 ]; then
+    run_command "delgroup $1" "Error modifying group $1"
+  fi
+
+  if [ $2 = yes ]; then
+    SYSTEM_GROUP="-S"
+  fi
+  if [ $# -gt 2 ]; then
+    GROUP_GID="-g $3"
+  fi  
+  run_command "addgroup $GROUP_GID $SYSTEM_GROUP $1" "Error adding group $1"
+  log_message INFO "Group <$1 ($(cat /etc/group | grep "$1"))>"
+}
+
+create_user() {
+# create_user login uid gid sudo shell create_home is_system extra_grops
+  COMMAND=""
+  if [ $(cat /etc/passwd | grep "$1" | wc -c) != 0 ]; then
+    run_command "deluser $1" "Error modifying user $1"
+  fi
+
+  COMMAND="adduser -u $2 -D"
+  if [ $# -gt 7 ] || [ $4 = yes ]; then
+    if [ $# -gt 7 ] && [ $4 = no ]; then
+      COMMAND="$COMMAND -G $8"
+    elif [ $# -gt 7 ] && [ $4 = yes ]; then
+      COMMAND="$COMMAND -G $8,sudo"
+    elif [ $# -eq 7 ] && [ $4 = yes ]; then
+      COMMAND="$COMMAND -G sudo"
+    fi
+  fi
+  COMMAND="$COMMAND -s $5"
+  if [ $6 != yes ]; then
+    COMMAND="$COMMAND -H"
+  fi
+  if [ $7 = yes ]; then
+    COMMAND="$COMMAND -S"
+  fi
+  COMMAND="$COMMAND $1 $3"
+  
+  run_command "$COMMAND" "Error adding user $1"
+
+  log_message READ "Set password for user <$1>"
+  run_command "passwd $1" "Error adding user $1"
+  log_message INFO "User <$1 ($(cat /etc/passwd | grep "$1"))>"
+}
