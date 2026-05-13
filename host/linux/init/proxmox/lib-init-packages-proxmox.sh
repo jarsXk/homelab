366b049f6a7b1aad31764bcab1dc665350a078f0
jarsXk <fedrrr@gmail.com>
2026-05-13 14:33:38 +0300

    Update lib-init-packages-proxmox.sh

--- host/linux/init/proxmox/lib-init-packages-proxmox.sh
+++ host/linux/init/proxmox/lib-init-packages-proxmox.sh
@@ -1,11 +1,11 @@
 
 # Updating
 log_message INFO "Updating packages"
-run_command "$PACKAGEMANAGER update" "Error updating"
-run_command "$PACKAGEMANAGER -y full-upgrade" "Error updating"
+run_command "apt update" "Error updating"
+run_command "apt -y full-upgrade" "Error updating"
 
 # Installing packages
 log_message "DEBUG" "DEBIAN_VERSION <$DISTRO_VERSION>"
 PACKAGE_LIST="micro mc htop curl wget zstd unzip sudo figlet dnsutils imagemagick iperf3 fastfetch"
 log_message INFO "Installing packages <$PACKAGE_LIST>"
-run_command "$PACKAGEMANAGER -y install $PACKAGE_LIST" "Error installing"
+run_command "apt -y install $PACKAGE_LIST" "Error installing"
